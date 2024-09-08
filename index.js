8cabcf93ecee7a9cc6465930b58fc19ddfa27db2
Janari Stserbina <janari.stserbina@gmail.com>
2024-09-08 16:23:41 +0300

    Read data from file

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const express = require('express')
 const app = express()
-
+const fs = require('fs')
 const path = require('path')
 app.set('view-engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
 
 app.get('/', (req, res) => {
-    res.render('index.ejs')
+
+    fs.readFile('./tasks', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err)
+            return
+        }
+        console.log(data)
+        const tasks = data.split('\n')
+        res.render('index.ejs', {tasks: tasks})
+    })
 })
 
 
