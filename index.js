b400351af4ec98e577b9a3c7b96da384aeeeac90
Janari Stserbina <janari.stserbina@gmail.com>
2024-09-08 16:53:16 +0300

    Write data to file

--- index.js
+++ index.js
@@ -2,20 +2,44 @@ const express = require('express')
 const app = express()
 const fs = require('fs')
 const path = require('path')
+
 app.set('view-engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
+app.use(express.urlencoded({ extended: true }));
+
+const readFile = filename => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(filename, 'utf8', (err, data) => {
+            if (err) {
+                reject(err)
+                return
+            }
+            // console.log(data)
+            const tasks = data.split('\n')
+            resolve(tasks)
+        })
+    })
+}
 
 
 app.get('/', (req, res) => {
 
-    fs.readFile('./tasks', 'utf8', (err, data) => {
-        if (err) {
-            console.error(err)
-            return
-        }
-        console.log(data)
-        const tasks = data.split('\n')
+    readFile('./tasks').then(tasks => {
         res.render('index.ejs', {tasks: tasks})
+    })
+})
+
+app.post('/', (req, res) => {
+    readFile('./tasks').then(tasks => {
+        tasks.push(req.body.task)
+        const data = tasks.join('\n')
+        fs.writeFile('./tasks', data, err => {
+            if (err) {
+                console.error(err)
+                return
+            }
+            res.redirect('/')
+        })
     })
 })
 
