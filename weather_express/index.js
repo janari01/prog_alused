51ff7df8023edb7f0f94b24a38d7d57a63786c73
janari01 <janari.stserbina@voco.ee>
2024-09-23 08:51:45 +0300

    Created project basic files

---
+++ weather_express/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+app.get('/', (req, res) =>{
+    res.render('index.ejs')
+})
+
+app.listen(3000)
