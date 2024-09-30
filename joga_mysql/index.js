43a5a6f56599b4155f5fe07d2a1024dd75b37a1f
Janari Stserbina <janari.stserbina@gmail.com>
2024-09-30 15:44:17 +0300

    Added handlebars template engine

--- joga_mysql/index.js
+++ joga_mysql/index.js
@@ -3,6 +3,17 @@ const express = require('express')
 const app = express()
 
 const path = require('path')
+
+const hbs = require('express-handlebars')
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'hbs')
+app.engine('hbs', hbs.engine({
+    extname: 'hbs',
+    defaultLayout: 'main',
+    layoutsDir: __dirname + '/views/layouts/'
+}))
+
 const mysql = require('mysql2')
 const bodyParser = require('body-parser')
 
