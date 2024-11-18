4d9ee3ae1c50ce672bcda152a7635229b92815d3
Janari Stserbina <janari.stserbina@gmail.com>
2024-11-18 22:22:09 +0200

    Change dirs

--- react/expenses/expenses_app/src/App.js
+++ react/expenses/expenses_app/src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 
-import Expenses from "./components/Expenses";
+import Expenses from "./components/Expenses/Expenses";
 
 function App() {
   const expenses = [
