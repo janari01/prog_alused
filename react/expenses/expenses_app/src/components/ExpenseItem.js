0f590174c0b566e715bbc1830544d289ea2c8484
Janari Stserbina <janari.stserbina@gmail.com>
2024-11-18 22:04:52 +0200

    Added className

--- react/expenses/expenses_app/src/components/ExpenseItem.js
+++ react/expenses/expenses_app/src/components/ExpenseItem.js
@@ -3,7 +3,7 @@ import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem(props){
     return (
-        <div className="expense-item">
+        <div className="card expense-item">
             <ExpenseDate date={props.expenseData.date}></ExpenseDate>
             <div className="expense-item__description">
                 <h2>{props.expenseData.title}</h2>
