c91f5de0c55397449a1a4b1db412ff669e18d0d5
Janari Stserbina <janari.stserbina@gmail.com>
2024-11-26 22:56:10 +0200

    Created project basic files

---
+++ todo-ts-oop/src/app.ts
@@ -0,0 +1,11 @@
+import express, { Request, Response, NextFunction } from 'express'
+import todoRoutes from './routes/todos'
+const app = express()
+
+app.use('/todos', todoRoutes)
+
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+    res.json({message: error.message})
+})
+
+app.listen(3011, () => console.log('server running...'))
