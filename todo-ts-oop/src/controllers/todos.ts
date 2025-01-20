663f46867984bcfaa08bf1c16d139ab58a679d9f
Janari Stserbina <janari.stserbina@gmail.com>
2025-01-20 18:25:25 +0200

    Ülesanne 10 - todo-oop-ts projekti teostamine ja esitlemine

--- todo-ts-oop/src/controllers/todos.ts
+++ todo-ts-oop/src/controllers/todos.ts
@@ -1,68 +1,75 @@
 import { Request, Response, NextFunction } from "express";
 import { Todo } from "../models/todo";
 
-const todos: Todo[] = []
-
-export const createTodo = (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const task = (req.body as {task: string}).task
-    const newTodo = new Todo(Math.random().toString(), task)
-    todos.push(newTodo)
-    res.status(201).json({
-      message: 'Created new todo',
-      createdtask: newTodo
-    })
-  } catch(err) {
-    console.log(err)
+class todoController {
+  todos: Todo[];
+  constructor(todos: Todo[] = []) {
+    this.todos = todos
   }
-}
 
-export const getTodos = (req: Request, res: Response, next: NextFunction) => {
-  try {
-    res.status(201).json({
-      tasks: todos
+  createTodo = (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const task = (req.body as {task: string}).task
+      const newTodo = new Todo(Math.random().toString(), task)
+      this.todos.push(newTodo)
+      res.status(201).json({
+        message: 'Created new todo',
+        createdtask: newTodo
     })
-  } catch(err) {
-    console.log(err)
-  }
-}
-
-export const updateTodo = (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const todoId = req.params.id
-    const updatedTask = (req.body as {task: string}).task
-    const todoIndex = todos.findIndex(todo => todo.id === todoId)
-
-    if (todoIndex < 0) {
-      throw new Error('Id not correct/missing')
+    } catch(err) {
+      console.log(err)
     }
-
-    todos[todoIndex] = new Todo(todos[todoIndex].id, updatedTask)
-
-    res.status(201).json({
-      message: 'Updated',
-      updatedTask: todos[todoIndex]
-    })
-  } catch(err) {
-    console.log(err)
   }
-}
 
-export const deleteTodo = (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const todoId = req.params.id
-    const todoIndex = todos.findIndex(todo => todo.id === todoId)
-
-    if (todoIndex < 0) {
-      throw new Error('Id not correct/missing')
+  getTodos = (req: Request, res: Response, next: NextFunction) => {
+    try {
+      res.status(201).json({
+        tasks: this.todos
+      })
+    } catch(err) {
+      console.log(err)
     }
+  }
 
-    todos.splice(todoIndex, 1)
+  updateTodo = (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const todoId = req.params.id
+      const updatedTask = (req.body as {task: string}).task
+      const todoIndex = this.todos.findIndex(todo => todo.id === todoId)
+  
+      if (todoIndex < 0) {
+        throw new Error('Id not correct/missing')
+      }
+  
+      this.todos[todoIndex] = new Todo(this.todos[todoIndex].id, updatedTask)
+  
+      res.status(201).json({
+        message: 'Updated',
+        updatedTask: this.todos[todoIndex]
+      })
+    } catch(err) {
+      console.log(err)
+    }
+  }
 
-    res.status(201).json({
-      message: 'Deleted',
-    })
-  } catch(err) {
-    console.log(err)
+  deleteTodo = (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const todoId = req.params.id
+      const todoIndex = this.todos.findIndex(todo => todo.id === todoId)
+  
+      if (todoIndex < 0) {
+        throw new Error('Id not correct/missing')
+      }
+  
+      this.todos.splice(todoIndex, 1)
+  
+      res.status(201).json({
+        message: 'Deleted',
+      })
+    } catch(err) {
+      console.log(err)
+    }
   }
 }
+
+export const TodoController = new todoController()
--- todo-ts-oop/src/routes/todos.ts
+++ todo-ts-oop/src/routes/todos.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express'
-import { createTodo, getTodos, updateTodo, deleteTodo } from '../controllers/todos'
+import { TodoController } from '../controllers/todos'
 
 const router = Router()
 
-router.post('/', createTodo)
-router.get('/', getTodos)
-router.patch('/:id', updateTodo)
-router.delete('/:id', deleteTodo)
+router.post('/', (req, res, next) => TodoController.createTodo(req, res, next))
+router.get('/', (req, res, next) => TodoController.getTodos(req, res, next))
+router.patch('/:id', (req, res, next) => TodoController.updateTodo(req, res, next))
+router.delete('/:id', (req, res, next) => TodoController.deleteTodo(req, res, next))
 
 export default router
