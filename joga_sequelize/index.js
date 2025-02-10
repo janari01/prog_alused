83e657e22799cf34f303f198ad6bc78684dbebca
Janari Stserbina <janari.stserbina@gmail.com>
2025-02-10 13:25:34 +0200

    Created connection to database

--- joga_sequelize/index.js
+++ joga_sequelize/index.js
@@ -1,8 +1,21 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+const Sequelize = require('sequelize')
+const sequelize = new Sequelize(process.env.URI)
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('connected to db')
+  })
+  .catch(err => {
+    console.error(err)
+  })
 
 app.get('/', (req, res) => {
   res.json({message: 'welcome'})
